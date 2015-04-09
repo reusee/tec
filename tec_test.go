94aa40efa60bcfb0fea0fda711801682090f1c22
reus <reusee@gmail.com>
2015-04-09 13:11:26 +0800

    more tec test

--- tec_test.go
+++ tec_test.go
@@ -1,8 +1,19 @@
 package tec
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/reusee/dms"
+)
 
 func TestNew(t *testing.T) {
 	c := New()
 	defer c.Close()
+	loaded := false
+	c.Load(mod{func(loader dms.Loader) {
+		loaded = true
+	}})
+	if !loaded {
+		t.Fatal("load")
+	}
 }
